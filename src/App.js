c2ee4a613c8919fd69a801e214ec7e424bbbe206
irumdev <jklsj1252@gmail.com>
2022-09-06 23:14:23 +0900

    feat: Add keyword validation check

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, {useState, useEffect} from 'react';
 
+const HelpText = (props) => {
+  return (
+    <label>{props.value}</label>
+  )
+}
+
 const SearchForm = () => {
   const [keyword, setKeyword] = useState('')
+  const [helpText, setHelpText] = useState('')
+  useEffect(() => {
+    const regex = /^[a-zA-Z0-9 ]*$/
+
+    if(regex.test(keyword)){
+      setHelpText('')
+    } else {
+      setHelpText('영어, 숫자, 띄어쓰기만 입력 가능')
+    }
+  });
 
   return (
     <form>
-      <input
-        type="text"
-        name="keyword"
-        value={keyword}
-        onChange={() => setKeyword(keyword)}
-      />
+      <input type="text" name="keyword" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+      <HelpText value={helpText}/>
       <button type="submit">검색</button>
     </form>
   )
