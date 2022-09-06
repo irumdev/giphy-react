282d082873a1c41855eba664f5844b6a19275306
irumdev <jklsj1252@gmail.com>
2022-09-06 21:30:23 +0900

    feat: Add SearchForm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+
+const SearchForm = () => {
+  const [keyword, setKeyword] = useState('')
+
+  return (
+    <form>
+      <input
+        type="text"
+        name="keyword"
+        value={keyword}
+        onChange={() => setKeyword(keyword)}
+      />
+      <button type="submit">검색</button>
+    </form>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<SearchForm />);
 }
 
 export default App;
